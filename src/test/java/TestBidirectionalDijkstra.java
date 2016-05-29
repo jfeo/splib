77629bf121087467c0eb920ab6e1185269b903ca
jfeo <xws747@alumni.ku.dk>
2016-05-29 10:07:40 +0200

    Removed 'static' test. Substantial changes to comparative test - dump svg representations of graph on both failure and success

--- src/test/java/TestBidirectionalDijkstra.java
+++ src/test/java/TestBidirectionalDijkstra.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import splib.util.Pair;
 import splib.util.GraphCreator;
 import splib.algo.BidirectionalDijkstra;
@@ -15,72 +18,54 @@ import splib.data.Vertex;
 
 public class TestBidirectionalDijkstra {
 
-  static private double DELTA = 1e-15;
-
-  @Test
-  public void test_singlePair() {
-    Graph<BDDVertex> G = new Graph<BDDVertex>();
-
-    BDDVertex v1 = new BDDVertex();
-    G.addVertex(v1);
-    BDDVertex v2 = new BDDVertex();
-    G.addVertex(v2);
-    BDDVertex v3 = new BDDVertex();
-    G.addVertex(v3);
-    BDDVertex v4 = new BDDVertex();
-    G.addVertex(v4);
-    BDDVertex v5 = new BDDVertex();
-    G.addVertex(v5);
-
-    G.addEdge(0, 1, 3.0);
-    G.addEdge(0, 4, 10.0);
-    G.addEdge(1, 4, 5.0);
-    G.addEdge(1, 3, 4.0);
-    G.addEdge(1, 2, 1.0);
-    G.addEdge(2, 3, 2.0);
-    G.addEdge(3, 4, 1.0);
-
-    double length;
-
-    length = BidirectionalDijkstra.singlePair(G, v1, v2, 3);
-    assertEquals(3.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v1, v4, 3);
-    assertEquals(6.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v1, v5, 3);
-    assertEquals(7.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v3, v5, 3);
-    assertEquals(3.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v2, v4, 3);
-    assertEquals(3.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v5, v2, 3);
-    assertEquals(4.0, length, DELTA);
-
-    length = BidirectionalDijkstra.singlePair(G, v4, v5, 3);
-    assertEquals(1.0, length, DELTA);
-  }
+  static private double DELTA = 1e-8;
 
   @Test
   public void test_dijkstraCompare() throws IllegalAccessException, InstantiationException {
     Graph<BDDVertex> G;
     BDDVertex s, t;
-    Double length;
+    Pair<Double, BDDVertex> result;
 
-    for (int i = 50; i <= 1000; i += 50) {
-      float p = 1.0f;
-      for (int j = 0; j < 10; j++) {
-        p = p / 10.0f;
-        System.out.println("Testing BDD on Erdos-Renyi(" + i + ", " + p + ")");
-        G = GraphCreator.erdosrenyi(BDDVertex.class, i, p);
+    int num = 1;
+    for (int i = 100; i <= 100; i += 10) {
+      for (int d = 3; d < 10; d++) {
+        Pair<Graph<BDDVertex>, ArrayList<Pair<Double, Double>>> planar = GraphCreator.planar(BDDVertex.class, 100, i, d);
+        G = planar.getItem1();
         s = G.getVertices().get(0);
         t = G.getVertices().get(1);
-        length = BidirectionalDijkstra.<BDDVertex>singlePair(G, s, t, 4);
+
         Dijkstra.<BDDVertex>singleSource(G, s, 4);
-        assertEquals(t.getEstimate(), length, DELTA);
+        ArrayList<BDDVertex> dijkstraPath = new ArrayList();
+        BDDVertex v = t;
+        while (v != null) {
+          dijkstraPath.add(v);
+          v = (BDDVertex)v.getPredecessor();
+        }
+        double dijkstraDistance = t.getEstimate();
+
+        result = BidirectionalDijkstra.<BDDVertex>singlePair(G, s, t, 4);
+
+        // Generate path as list of vertices
+        ArrayList<BDDVertex> bddPath = new ArrayList();
+        v = result.getItem2();
+        while (v != null) {
+          bddPath.add(v);
+          v = (BDDVertex)v.getPredecessor();
+        }
+        Collections.reverse(bddPath);
+        v = result.getItem2().getSuccessor();
+        while (v != null) {
+          bddPath.add(v);
+          v = v.getSuccessor();
+        }
+
+        if (Math.abs(dijkstraDistance - result.getItem1()) > DELTA) {
+          GraphCreator.<BDDVertex>dumpTestSvg(100, "bdd_test_graph_fail_"+num+".svg", G, planar.getItem2(), s, t, bddPath, dijkstraPath, result.getItem2());
+        } else {
+          GraphCreator.<BDDVertex>dumpTestSvg(100, "bdd_test_graph_success_"+num+".svg", G, planar.getItem2(), s, t, bddPath, dijkstraPath, result.getItem2());
+        }
+        num++;
+        assertEquals(dijkstraDistance, result.getItem1(), DELTA);
       }
     }
 
