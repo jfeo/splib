ea369276232b40ecb7a286ae54399f6a9637d921
jfeo <xws747@alumni.ku.dk>
2016-06-08 22:39:01 +0200

    Adapt benchmarks to changes in graph creator

--- src/main/java/splib/run/BenchmarkBidirectionalDijkstra.java
+++ src/main/java/splib/run/BenchmarkBidirectionalDijkstra.java
@@ -17,18 +17,18 @@ public class BenchmarkBidirectionalDijkstra {
 
 
   public static void main(String[] arg) throws InstantiationException, IllegalAccessException {
-    BenchmarkSuite<BDDVertex> b = new BenchmarkSuite<BDDVertex>();
+    BenchmarkSuite<BDDVertex> b = new BenchmarkSuite<BDDVertex>(BDDVertex.class);
     int maxArity = 10;
 
 
     for (int i = 100; i < 4600; i += 100) {
-      for (float p = 1.0f; p > 2.0f * Math.log((double)i) / (double)i; p -= 0.07f) {
+      for (float p = 1.0f; p > 2.0f * Math.log((double)i) / (double)i; p -= 0.10f) {
         Graph<BDDVertex> G = GraphCreator.<BDDVertex>erdosrenyi(BDDVertex.class, i, p);
         for (int a = 2; a <= maxArity; a++) {
-            b.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
-                "Bidirectional Dijkstra, " + a + "-ary Heap", BidirectionalDijkstra::singlePair,
-                new Quad(G, G.getVertices().get(0), G.getVertices().get(1) , a));
-          }
+          b.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
+            "Bidirectional Dijkstra, " + a + "-ary Heap", BidirectionalDijkstra::singlePair,
+            new Quad(G, G.getVertices().get(0), G.getVertices().get(1) , a));
+        }
       }
     }
 
--- src/main/java/splib/run/BenchmarkDijkstra.java
+++ src/main/java/splib/run/BenchmarkDijkstra.java
@@ -18,7 +18,7 @@ public class BenchmarkDijkstra {
 
   public static void main(String[] arg) throws InstantiationException, IllegalAccessException {
     //Warm-up
-    BenchmarkSuite<SPVertex> b = new BenchmarkSuite<SPVertex>();
+    BenchmarkSuite<SPVertex> b = new BenchmarkSuite<SPVertex>(SPVertex.class);
     for (int i = 0; i < 10000; i++) {
       Graph<SPVertex> G = GraphCreator.complete(SPVertex.class, 50);
       b.addSingleSourceBenchmark("", Dijkstra::singleSource, G,
@@ -30,28 +30,18 @@ public class BenchmarkDijkstra {
 
     // Warm-up complete
     // BenchmarkSuite<SPVertex> b = new BenchmarkSuite<SPVertex>();
-    b = new BenchmarkSuite<SPVertex>();
+    b = new BenchmarkSuite<SPVertex>(SPVertex.class);
     int numHeaps = 10;
 
-    for (int i = 500; i < 5000; i += 500) {
-      for (int d = 2; d < 10; d++) {
+
+    for (int i = 150; i < 5000; i += 150) {
+      for (int d = 2; d < i / 2; d *= 3) {
         Pair<Graph<SPVertex>, ?> G1 = GraphCreator.<SPVertex>euclidian(SPVertex.class, 100d, i, d);
         for (int a = 2; a <= numHeaps; a++) {
           b.runSingleSourceBenchmark(BenchmarkSuite.Output.CSV,
               "Dijkstra, " + a + "-ary Heap", Dijkstra::singleSource,
               new Triple(G1.getItem1(), G1.getItem1().getVertices().get(10), a));
         }
-      }
-    }
-
-    for (int i = 100; i < 4600; i += 100) {
-      for (float p = 1.0f; p > 2.0f * Math.log((double)i) / (double)i; p -= 0.05f) {
-        Graph<SPVertex> G = GraphCreator.<SPVertex>erdosrenyi(SPVertex.class, i, p);
-        for (int a = 2; a <= numHeaps; a++) {
-          b.runSingleSourceBenchmark(BenchmarkSuite.Output.CSV,
-              "Dijkstra, " + a + "-ary Heap", Dijkstra::singleSource,
-              new Triple(G, G.getVertices().get(0), a));
-          }
       }
     }
   }
