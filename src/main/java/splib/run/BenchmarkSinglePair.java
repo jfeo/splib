4a00a0fdcc264829a13ac0e4c895fc735241d993
jfeo <xws747@alumni.ku.dk>
2016-06-08 22:40:31 +0200

    Benchmark A* and BDD on the same graphs with the same vertices

--- src/main/java/splib/run/BenchmarkSinglePair.java
+++ src/main/java/splib/run/BenchmarkSinglePair.java
@@ -11,6 +11,7 @@ import splib.util.GraphCreator;
 import splib.util.Pair;
 import splib.util.Quad;
 import splib.data.BDDVertex;
+import splib.data.EuclidianSPVertex;
 import splib.data.Graph;
 
 public class BenchmarkSinglePair {
@@ -18,45 +19,33 @@ public class BenchmarkSinglePair {
 
   public static void main(String[] arg) throws InstantiationException, IllegalAccessException {
 
-    // BenchmarkSuite.SinglePairAlgorithm<EuclidianSPVertex> astar = (Gr, vs, vt, a) -> Astar.<EuclidianSPVertex>singlePair(Gr, vs, vt, Astar::euclidianHeuristic, a);
-    // BenchmarkSuite<BDDVertex> b = new BenchmarkSuite<BDDVertex>();
+    BenchmarkSuite.SinglePairAlgorithm<EuclidianSPVertex> astar = (Gr, vs, vt, a) -> Astar.<EuclidianSPVertex>singlePair(Gr, vs, vt, Astar::euclidianHeuristic, a);
 
-    // int maxArity = 10;
+    BenchmarkSuite<BDDVertex> b_bdd = new BenchmarkSuite<BDDVertex>(BDDVertex.class);
+    BenchmarkSuite<EuclidianSPVertex> b_astar = new BenchmarkSuite<EuclidianSPVertex>(EuclidianSPVertex.class);
 
-    // for (int i = 100; i < 4600; i += 100) {
-    //   for (float p = 1.0f; p > 2.0f * Math.log((double)i) / (double)i; p -= 0.01f) {
-    //     Graph<BDDVertex> G = GraphCreator.<BDDVertex>erdosrenyi(BDDVertex.class, i, p);
-    //     for (int a = 2; a <= maxArity; a++) {
-    //         for (int t = 1; t < 10; t++) {
+    int maxArity = 10;
 
-    //           b.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
-    //               "Bidirectional Dijkstra, " + a + "-ary Heap", BidirectionalDijkstra::singlePair,
-    //               new Quad(G, G.getVertices().get(0), G.getVertices().get(t) , a));
-    //         }
-    //       }
-    //   }
-    // }
-
-    // for (int i = 150; i < 5000; i += 100) {
-    //   for (int d = 2; d < i; d += 70) {
-    //     Pair<Graph<BDDVertex>, ?> G = GraphCreator.<BDDVertex>euclidian(BDDVertex.class, 100d, i, d);
-    //     for (int a = 2; a <= maxArity; a++) {
-    //       // run bdd on a bunch of pairs
-    //       for (int u = 1; u < i; u += i/5) {
-    //         b.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
-    //           "BidirectionalDijkstra " + a + "-ary Heap",
-    //           BidirectionalDijkstra::<BDDVertex>singlePair,
-    //           new Quad(G.getItem1(), G.getItem1().getVertices().get(0),
-    //             G.getItem1().getVertices().get(u), a));
-    //         b.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
-    //           "BidirectionalDijkstra " + a + "-ary Heap",
-    //           BidirectionalDijkstra::<BDDVertex>singlePair,
-    //           new Quad(G.getItem1(), G.getItem1().getVertices().get(0),
-    //             G.getItem1().getVertices().get(u), a));
-    //       }
-    //     }
-    //   }
-    // }
+    for (int i = 150; i < 5000; i += 150) {
+      for (int d = 2; d < i / 2; d *= 3) {
+        Pair<Graph<BDDVertex>, ArrayList<Pair<Double, Double>>> G_Bdd = GraphCreator.<BDDVertex>euclidian(BDDVertex.class, 100d, i, d);
+        Graph<EuclidianSPVertex> G_Astar = GraphCreator.<EuclidianSPVertex, BDDVertex>copy(EuclidianSPVertex.class, G_Bdd.getItem1(), G_Bdd.getItem2());
+        for (int a = 2; a <= maxArity; a++) {
+          // run bdd on a bunch of pairs
+          for (int u = 1; u < i; u += i/5) {
+            b_bdd.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
+              "BDD - " + (u+a),
+              BidirectionalDijkstra::<BDDVertex>singlePair,
+              new Quad(G_Bdd.getItem1(), G_Bdd.getItem1().getVertices().get(0),
+                G_Bdd.getItem1().getVertices().get(u), a));
+            b_astar.runSinglePairBenchmark(BenchmarkSuite.Output.CSV,
+              "A* - " + (u+a),
+              astar, new Quad(G_Astar, G_Astar.getVertices().get(0),
+                G_Astar.getVertices().get(u), a));
+          }
+        }
+      }
+    }
   }
 
 
